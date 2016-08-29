e697b8f297cc580b60f09c38e7dd886b31bfa6a6
Wen-Chun Lin <lovecankill@gmail.com>
2016-08-29 21:15:55 +0800

    Very basic repl

--- src/lispy/lib.rs
+++ src/lispy/lib.rs
@@ -1,4 +1,23 @@
+use std::io::{self, Write};
+
+/// A prompt-read-eval-print loop.
+fn repl(prompt: &str) {
+    loop {
+        let mut input = String::new();
+
+        print!("{}", prompt);
+        io::stdout().flush().unwrap();
+
+        match io::stdin().read_line(&mut input) {
+            Ok(_) => {
+                print!("{}", input);
+            }
+            Err(error) => println!("error: {}", error),
+        }
+    }
+}
+
 pub fn run() -> i32 {
-    println!("Hello world");
+    repl("lis.py> ");
     0
 }
