afebcc2369977acfe83e05313c42026e5a5767ff
Wen-Chun Lin <lovecankill@gmail.com>
2016-08-31 21:35:05 +0800

    Tokenize string

--- src/lispy/lib.rs
+++ src/lispy/lib.rs
@@ -1,19 +1,35 @@
 use std::io::{self, Write};
 
+/// Convert a string into a list of tokens.
+fn tokenize(s: &str) -> Vec<String> {
+    let replaced = s.replace("(", " ( ").replace(")", " ) ");
+    let v: Vec<String> = replaced.split_whitespace().map(String::from).collect();
+    v
+}
+
+fn parse(program: &str) {
+    print!("{:?}", tokenize(program));
+}
+
+fn read(prompt: &str) -> String {
+    let mut input = String::new();
+
+    print!("{}", prompt);
+    io::stdout().flush().unwrap();
+
+    match io::stdin().read_line(&mut input) {
+        Ok(_) => {
+            input
+        }
+        Err(error) => panic!("error: {}", error),
+    }
+}
+
 /// A prompt-read-eval-print loop.
 fn repl(prompt: &str) {
     loop {
-        let mut input = String::new();
-
-        print!("{}", prompt);
-        io::stdout().flush().unwrap();
-
-        match io::stdin().read_line(&mut input) {
-            Ok(_) => {
-                print!("{}", input);
-            }
-            Err(error) => println!("error: {}", error),
-        }
+        let program = read(prompt);
+        parse(&program);
     }
 }
 
